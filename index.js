c70f1048d33a1495ce700f708467bfd759985cfc
Lee <>
2024-07-31 07:25:51 +0700

    install better-sqlite3

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require("express")
+import express from "express"
 
 const app = express()
 
 app.get("/", function(request, response){
-    response.send("<h1>Hello</h1>")
+    response.json({title: "Once upon an Algorithm"})
 })
 
 app.listen(8000)
