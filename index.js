001d7f2b9931fe201ed2ddd4cd026c77f9a14ee8
Lee <>
2024-07-31 21:49:29 +0700

    update

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const app = express()
 app.use(express.json())
 
 
-app.get("/", function(request, response){
-    response.json({title: "Once upon an Algorithm"})
+app.get("/", function (request, response) {
+    response.json({ title: "Once upon an Algorithm" })
 })
 
-app.post("/book", function(request, response){
-    const {title, author} = request.body
+app.post("/book", function (request, response) {
+    const { title, author } = request.body
 
     const statement = db.prepare("INSERT INTO books (title, author) VALUES (?,?)")
     const result = statement.run(title, author)
@@ -18,16 +18,25 @@ app.post("/book", function(request, response){
     response.json(result)
 })
 
-app.get("/book", function (request, response){
+app.get("/book", function (request, response) {
     const statement = db.prepare("SELECT * FROM books")
     const result = statement.all()
     response.json(result)
 })
 
-app.get("/book/:id", function(request, response){
+app.get("/book/:id", function (request, response) {
     const { id } = request.params
-    const statement =  db.prepare("SELECT * FROM books WHERE id = ?")
+    const statement = db.prepare("SELECT * FROM books WHERE id = ?")
     const result = statement.get(id)
+    response.json(result)
+})
+
+app.patch("/book/:id", function (request, response) {
+    const { id } = request.params
+    const { title } = request.body
+
+    const statement = db.prepare("UPDATE books SET title = ? WHERE id = ?")
+    const result = statement.run(title, id)
     response.json(result)
 })
 
